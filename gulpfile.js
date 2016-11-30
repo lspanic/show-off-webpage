22cba2045839dbd87ecfe855133e70b5b110ee34
Leon Spanic <spanicleon7@gmail.com>
2016-11-30 17:55:32 +0100

    installed gulp, restructured project folders

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('styles', function() {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
+});
+
+//Watch task
+gulp.task('default',function() {
+    gulp.watch('sass/**/*.scss',['styles']);
+});
+
+/* 
+We pass in the path to the files we want to watch, and then pass in an array with the tasks 
+that we want to run when the files are changed. The great thing about this task is since we have called 
+this task ‘default’, we can just run gulp when we want to run gulp,
+ no need to specify a task! And it will now just sit and wait for files to be saved and then run our task!
+*/
